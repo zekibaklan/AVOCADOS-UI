35b0a40417846c39ea6c09809ca18fcdc5ddb895
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-14 21:15:38 +0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Zeki Baklan on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
